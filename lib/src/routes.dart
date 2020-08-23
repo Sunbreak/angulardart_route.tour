d024b8b5518c9524549668ece339dcf31a05ec70
Sunbreak <sunbreak.wang@gmail.com>
2020-08-23 12:51:03 +0800

    Default, Redirect and Wildcard Routes
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -2,6 +2,7 @@ import 'package:angular_router/angular_router.dart';
 
 import 'crisis_list_component.template.dart' as crisis_list_template;
 import 'hero_list_component.template.dart' as hero_list_template;
+import 'not_found_component.template.dart' as not_found_template;
 import 'route_paths.dart';
 
 export 'route_paths.dart';
@@ -20,5 +21,13 @@ class Routes {
   static final all = <RouteDefinition>[
     crises,
     heroes,
+    RouteDefinition.redirect(
+      path: '',
+      redirectTo: RoutePaths.heroes.toUrl(),
+    ),
+    RouteDefinition(
+      path: '.+',
+      component: not_found_template.NotFoundComponentNgFactory,
+    ),
   ];
 }
